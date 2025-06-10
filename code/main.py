1f5c9f13bc158628f0115d31ccc9e876d2b977b3
Santiago Torres <91999194+dstorresm@users.noreply.github.com>
2025-06-10 15:54:38 -0500

    Delete code directory

--- code/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-
-# Importa la función principal de la interfaz gráfica desde el módulo gui
-from gui import run_gui
-
-# Punto de entrada del programa.
-# Si este archivo se ejecuta directamente, se inicia la interfaz gráfica de usuario.
-if __name__ == "__main__":
-    run_gui()
